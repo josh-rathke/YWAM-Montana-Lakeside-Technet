b15d5fa9d8398e854c1055f1420fb7d6017734d7
Josh Rathke <josh.rathke@ywammontana.org>
2016-05-03 17:09:30 -0600

    Enable Featured Image Functionality on Home Page

--- page-templates/page_home_page.php
+++ page-templates/page_home_page.php
@@ -8,7 +8,7 @@
 
 get_header(); ?>
 
-    <div class="hero-header" data-parallax="scroll" data-image-src="http://localhost:8888/technet.ywammontana.org/wp-content/uploads/2016/05/photo-1448960968772-b63b3f40dfc1.jpeg">        
+    <div class="hero-header" data-parallax="scroll" data-image-src="<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID)); ?>">        
         <div class="hero-description row">
             <div class="hero-description-text">
                 <h2>Spreading the Gospel</h2>
