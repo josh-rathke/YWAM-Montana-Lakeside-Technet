4478c3dd13c59653fe37b30ca433a02a80fca042
Josh Rathke <josh.rathke@ywammontana.org>
2016-05-03 14:50:49 -0600

    Added Home Page Template

---
+++ page-templates/page_home_page.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ *  Home Page Template
+ *  Template Name: Home Page
+ *
+ */
+
+get_header(); ?>
+
+    <div class="hero-header" data-parallax="scroll" data-image-src="http://localhost:8888/technet.ywammontana.org/wp-content/uploads/2016/05/photo-1448960968772-b63b3f40dfc1.jpeg">        
+        <div class="hero-description row">
+            <div class="hero-description-text">
+                <h2>Spreading the Gospel</h2>
+                <h3>One Internet Connection At A Time</h3>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
